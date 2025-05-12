166a068386677fd3f207ec947c13301787ddc329
Ratan <ratanrathod870@gmail.com>
2025-05-12 20:53:19 +0530

    added navbar and nav elements

--- src/App.jsx
+++ src/App.jsx
@@ -67,8 +67,33 @@ const App = () => {
       </div>
 
       {showContent &&
-        <div className="main w-full h- bg-black ">
+        <div className="main w-full">
+          <div className="landing w-full h-screen overflow-hidden bg-black">
 
+            <div className=" absolute top-0 left-0 z-[10] navbar w-full py-10 px-10 ">
+              <div className="logo flex gap-5">
+                <div className="lines flex flex-col gap-[5px]">
+                  <div className="line w-15 h-2 bg-white "></div>
+                  <div className="line w-8 h-2 bg-white "></div>
+                  <div className="line w-5 h-2 bg-white "></div>
+                </div>
+                <h3 className='text-4xl text-white -mt-[8px] leading-none '>Rockstar</h3>
+              </div>
+            </div>
+      
+            <div className="images relative overflow-hidden w-full h-screen">
+              <img className='absolute top-0 left-0 w-full h-full object-cover'
+                src="./sky.png"
+                alt="" />
+              <img className='absolute top-0 left-0 w-full h-full object-cover'
+                src="./bg.png"
+                alt="" />
+              <img className='absolute bottom-[-40%] left-1/2 -translate-x-1/2 scale-[0.8]'
+                src="./girlbg.png"
+                alt="" />
+            </div>
+
+          </div>
         </div>
       }
 
