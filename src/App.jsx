f102e0d87e51d02a222369e152c7cb4d02eb4ff5
Ratan <ratanrathod870@gmail.com>
2025-05-12 20:24:31 +0530

    vi image animated using gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 const App = () => {
+
+  let [showContent, setShowContent] = useState(false)
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.to(".vi-mask-group", {
+      rotate: 10,
+      duration: 2,
+      ease: "Power4.easeInOut",
+      transformOrigin: "50% 50%",
+    }).to(".vi-mask-group", {
+      scale: 10,
+      duration: 2,
+      delay: -1.8,
+      ease: "Expo.easeInOut",
+      transformOrigin: "50% 50%",
+      opacity: 0,
+      onUpdate: function () {
+        if (this.progress() >= 0.9) {
+          document.querySelector(".svg").remove();
+          setShowContent(true);
+          this.kill();
+        }
+      }
+    })
+  })
+
+
+
   return (
     <>
-      <div className="svg fixed top-0 left-0 z-[2] w-full h-screen overflow-hidden bg-[#000]">
+      <div className="svg fixed flex items-center justify-center top-0 left-0 z-[1] w-full h-screen overflow-hidden bg-[#000]">
         <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
           <defs>
             <mask id="viMask">
